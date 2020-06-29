0ebce6f9e7f4d84b46c36bd7e8899171501f8e07
Quang giau <lequanggiau123@gmail.com>
2020-06-29 22:08:54 +0700

    not fix, update 29/6

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -12,88 +12,72 @@ export class EmployeeListComponent implements OnInit {
   employees = [];
   emp: employee;
   check = true;
-  constructor( private empService: EmployeeService,  private router : Router) { }
+  constructor(private empService: EmployeeService, private router: Router) { }
   ngOnInit() {
     this.getEmps();
   }
 
   /* Take data from serve --> employees*/
   getEmps(): void {
-   this.empService.getEmps().subscribe(
-     data => {
-      this.employees = data
-     }
+    this.empService.getEmps().subscribe(
+      data => {
+        this.employees = data
+      }
     ); // data sendto employees[]
   }
-  
-  Detail(empID: number){
+
+  Detail(empID: number) {
     this.router.navigate([`employee-detail/${empID}`]);
     // this.empService.getEmp(empID).subscribe(data => this.emp = data);
   }
 
-// **** Update employee *******/
-  Update(empID: number){
+  // **** Update employee *******/
+  Update(empID: number) {
     this.router.navigate([`employee-profile/${empID}`]);
     // employee profile == user frofile
   }
-// **** *************** *******/
+  // **** *************** *******/
 
   deleteEmp(empID: number) {
-
-   // if (confirm("do you want to delete?")) {
-      this.empService.deleteEmp(empID).subscribe(
-        data => {
-          //console.log('dsdsd', data);
-          //alert(`Delete Successful! ${data}`);
-          this.getEmps();
-        }, error => console.log(error)
-      );
-    //}
+    this.empService.deleteEmp(empID).subscribe(
+      data => {
+      },
+      error => console.log(error),
+      () => {
+        alert("delete succesfull!");
+        this.getEmps();
+      }
+    );
   }
+
   // Create new employee
-  New_Employee(){
+  New_Employee() {
     this.router.navigate(['create-employee']);
   }
 
-  // delete_muti_Emp_checkbox(){
-  //   for (let index = 0; index < this.save_temp.length; index++) {
-  //     const empID = this.save_temp[index];
-  //     this.empService.deleteEmp(empID).subscribe()
-  //   } 
-  //   this.getEmps();  
-  // }
-  checkauto:boolean = false;
-  save:string;
-  save_temp= [];
+  checkauto: boolean = false;
+  save: string;
+  save_temp = [];
   SaveIDEmp(event: Event) {
-    this.checkauto= (event.target as HTMLInputElement).checked;
-    if(this.checkauto == true)
-    {  
-          this.save += (event.target as HTMLInputElement).value+',';
+    this.checkauto = (event.target as HTMLInputElement).checked;
+    if (this.checkauto == true) {
+      this.save += (event.target as HTMLInputElement).value + ',';
     }
-    else
-    {
-       this.save = this.save.replace((event.target as HTMLInputElement).value+',',"");
-         }
+    else {
+      this.save = this.save.replace((event.target as HTMLInputElement).value + ',', "");
+    }
     //alert(this.save.replace("undefined",""));
-   this.save = this.save.replace("undefined","");
-   // console.log('checkbox_value',this.save);
+    this.save = this.save.replace("undefined", "");
+    // console.log('checkbox_value',this.save);
     this.save_temp = this.save.split(',');
+  }
 
-   console.log('checkbox_value',this.save_temp);
-
-   console.log("checkk", event.target as HTMLInputElement);
-   
-   // console.log('checkbox_value',this.save);
- }
-
-  delete_muti_Emp_checkbox(){
-    console.log('xx',this.save_temp);
-   this.empService.delete_muti_Emp_checkbox(this.save_temp).subscribe(
-    data => {
+  delete_muti_Emp_checkbox() {
+    console.log('xx', this.save_temp);
+    this.empService.delete_muti_Emp_checkbox(this.save_temp).subscribe(
+      data => {
         alert("delete succesfull!");
-      }
-      , 
+      },
       error => {
         console.log(error)
         alert("delete failed!");
@@ -102,6 +86,6 @@ export class EmployeeListComponent implements OnInit {
         this.getEmps();
         this.save = "";
       }
-   );
+    );
   }
 }
--- src/app/employee-profile/employee-profile.component.ts
+++ src/app/employee-profile/employee-profile.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { employee } from '../employee';
 import { ActivatedRoute, Router } from '@angular/router';
 import { EmployeeService } from '../employee.service';
-import {FormControl, FormGroup, Validators, FormBuilder, FormArray} from '@angular/forms';
+import { FormControl, FormGroup, Validators, FormBuilder, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-employee-profile',
@@ -19,41 +19,39 @@ export class EmployeeProfileComponent implements OnInit {
   myControl_Language = new FormControl();
   skills = new FormControl();
   option_genders: string[] = ['Male', 'FeMale', 'Other'];
-  option_departments: string[] = ['GA', 'DEV-EMS', 'DEV-BAS', 'BPO','DTP','Other'];
+  option_departments: string[] = ['GA', 'DEV-EMS', 'DEV-BAS', 'BPO', 'DTP', 'Other'];
   option_projects: string[] = ['Other', 'OJT', 'Offshore', 'OJT-Training'];
-  option_languages: string[] = ['None', 'English','JPN N5','JPN N4','JPN N3','JPN N2','JPN N1','Other'];
+  option_languages: string[] = ['None', 'English', 'JPN N5', 'JPN N4', 'JPN N3', 'JPN N2', 'JPN N1', 'Other'];
   // end Set selection for gender employee
   skillLists: string[] = ['Java', 'PHP', 'Ruby', 'Python', 'ReacJS', 'Other'];
-  skillEmployee : string[];
-  
+  skillEmployee: string[];
+
   constructor(private route: ActivatedRoute, private router: Router, private employeeService: EmployeeService) { }
-  
+
   ngOnInit() {
     this.emp = new employee();
     this.id = this.route.snapshot.params['empId'];
-    //console.log(this.id);
     this.employeeService.getEmp(this.id).subscribe(
-      data => { this.emp = data;
-                //console.log('update', this.emp) 
-                this.skillEmployee =  this.emp.empSkills.split(', '); // --> mang
-                //console.log(this.skillEmployee);
-                //console.log('befor',this.skillLists);
-      } 
-      
-    );
-   // console.log('aff',this.skillLists);
-    
+      data => {
+        this.emp = data;
+        if (this.emp.empSkills != null) {
+          this.skillEmployee = this.emp.empSkills.split(', ');
+        }
+      },
+      err => console.error('Observer got an error: ' + err),
+      () => console.log('Observer got a complete notification')
+    )
   }
-  Update(){
-    if(this.skills.value != null) {
+  
+  Update() {
+    if (this.skills.value != null) {
       this.emp.empSkills = this.skills.value.join(', ');
-   // console.log('skill after update', this.emp.empSkills );
-    this.employeeService.updateEmp(this.id, this.emp).subscribe(
-      err => console.log(err)
-    );
-    //this.employeeService.getEmps().subscribe();
+      this.employeeService.updateEmp(this.id, this.emp).subscribe(
+        x => console.log('Observer got a next value: ' + x),
+        err => console.error('Observer got an error: ' + err),
+        () => console.log('Observer got a complete notification')
+      );
     }
-    
     this.router.navigate(['employee-list'])
   }
 }
